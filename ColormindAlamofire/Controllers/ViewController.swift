b7bf4f2319a68484ebe7ed6bfc9001d2fb7c4b69
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-21 18:30:09 +0300

    make updateUI Button

--- ColormindAlamofire/Controllers/ViewController.swift
+++ ColormindAlamofire/Controllers/ViewController.swift
@@ -11,11 +11,6 @@ import Alamofire
 class ViewController: UIViewController {
 
     @IBOutlet var paletteViews: [UIView]!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
 
     @IBAction func buttonPressed() {
         let url = "http://colormind.io/api/"
@@ -25,11 +20,26 @@ class ViewController: UIViewController {
             .responseDecodable(of: PaletteJSONData.self) { responseData in
                 switch responseData.result {
                 case .success(let value):
-                    print("AFvalue", value)
+                    let pallete = Palette(paletteJSONDataResult: value.result)
+                    DispatchQueue.main.async {
+                        self.updateUI(with: pallete)
+                    }
                 case .failure(let error):
                     print("AFerror", error)
                 }
             }
+    }
+    
+    private func updateUI(with palette: Palette) {
+        zip(paletteViews, palette.colors).forEach { (view, color) in
+            
+            let colorUI = UIColor(red: CGFloat(color[.red]!),
+                                  green: CGFloat(color[.green]!),
+                                  blue: CGFloat(color[.blue]!),
+                                                alpha: 1.0)
+            
+            view.backgroundColor = colorUI
+        }
     }
 }
 
--- ColormindAlamofire/Models/Palette.swift
+++ ColormindAlamofire/Models/Palette.swift
@@ -10,8 +10,23 @@ struct PaletteJSONData: Decodable {
 }
 
 struct Palette {
+    enum ColorSet: String, CaseIterable {
+        case red, green, blue
+    }
     
-    
-    let colors: [Int]
-    
+    var colors: [[ColorSet: Int]]
+                 
+    init(paletteJSONDataResult: [[Int]]) {
+        var colors = [[Palette.ColorSet: Int]]()
+        
+        for paletteColors in paletteJSONDataResult {
+            var color = [Palette.ColorSet: Int]()
+            for index in 0..<paletteColors.count {
+                color[ColorSet.allCases[index]] = paletteColors[index]
+            }
+            colors.append(color)
+        }
+        
+        self.colors = colors
+    }
 }
