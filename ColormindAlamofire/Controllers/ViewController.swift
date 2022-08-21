1f0a71b78b08392c96c271932767d52466e7950f
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-21 19:11:55 +0300

    complete

--- ColormindAlamofire/Controllers/ViewController.swift
+++ ColormindAlamofire/Controllers/ViewController.swift
@@ -11,6 +11,8 @@ import Alamofire
 class ViewController: UIViewController {
 
     @IBOutlet var paletteViews: [UIView]!
+    @IBOutlet var paletteLabels: [UILabel]!
+    
 
     @IBAction func buttonPressed() {
         let url = "http://colormind.io/api/"
@@ -31,6 +33,38 @@ class ViewController: UIViewController {
     }
     
     private func updateUI(with palette: Palette) {
+        
+        let maxInterationNumber = max(palette.colors.count,
+                                      paletteViews.count,
+                                      paletteLabels.count)
+        
+        for index in 0..<maxInterationNumber {
+            
+            let color = palette.colors[index]
+            let colorUI = UIColor(red: getCGFloatColorNumber(from: color[.red]),
+                                  green: getCGFloatColorNumber(from: color[.green]),
+                                  blue: getCGFloatColorNumber(from: color[.blue]),
+                                  alpha: 1.0)
+            
+            var red: CGFloat = 0
+            var green: CGFloat = 0
+            var blue: CGFloat = 0
+            colorUI.getRed(&red, green: &green, blue: &blue, alpha: nil)
+            
+            red = round(red * 1000) / 1000
+            green = round(red * 1000) / 1000
+            blue = round(red * 1000) / 1000
+            
+            UIView.animate(withDuration: 0.5) {
+                
+                self.paletteViews[index].backgroundColor = colorUI
+                self.paletteLabels[index].text = "red: \(red), green: \(green), blue: \(blue)"
+                
+            }
+        }
+        
+        
+        /*
         zip(paletteViews, palette.colors).forEach { (view, color) in
             let colorUI = UIColor(red: getCGFloatColorNumber(from: color[.red]),
                                   green: getCGFloatColorNumber(from: color[.green]),
@@ -41,6 +75,7 @@ class ViewController: UIViewController {
                 view.backgroundColor = colorUI
             }
         }
+        */
     }
     
     private func getCGFloatColorNumber(from eightBitColorNumber: Int?) -> CGFloat {
